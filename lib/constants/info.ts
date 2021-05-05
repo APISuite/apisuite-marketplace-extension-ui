2668b57dcd2c1b669929fe9bd6d10d0d327eba58
Bruno Morgado <mstrk512@gmail.com>
2021-05-05 19:50:51 +0100

    1.0.5

--- lib/constants/info.ts
+++ lib/constants/info.ts
@@ -5,4 +5,4 @@
 /////////////////////////////////////////////////////////////////////////////////
 
 export const name = '@apisuite/apisuite-marketplace-extension-ui'
-export const version = '1.0.4'
+export const version = '1.0.5'
