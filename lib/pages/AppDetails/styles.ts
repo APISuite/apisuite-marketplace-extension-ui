4e57842a4251879490b822072ef867ada96bf9f9
Pedro Carvalho <pedrocloudoki@pedros-air.lan>
2021-05-21 15:51:45 +0100

    feat: Added ability to display uploaded public app images in the Marketplace's 'App Details' view

--- lib/pages/AppDetails/styles.ts
+++ lib/pages/AppDetails/styles.ts
@@ -54,12 +54,23 @@ export default makeStyles((theme) => ({
   },
 
   appImageGallery: {
-    '& .image-gallery-slides': {
-      borderRadius: '10px',
+    // Images
+    '& .image-gallery-content .image-gallery-slide .image-gallery-image': {
+      borderRadius: 10,
+      height: 435,
     },
 
+    // Thumbnails
+    '& .image-gallery-thumbnail .image-gallery-thumbnail-image': {
+      height: 75,
+      lineHeight: 0,
+      verticalAlign: 'middle',
+      width: 'auto',
+    },
+
+    // Selected thumbnail
     '& .active': {
-      border: `4px solid theme.palette.action.focus`,
+      border: `4px solid ${theme.palette.action.focus}`,
     },
   },
 
