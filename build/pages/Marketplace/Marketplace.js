8b3aa9d052eddaf624c3078ff9ce6d55992b37ba
a31859 <a31859@users.noreply.github.com>
2021-06-09 14:43:26 +0100

    1.0.14

--- build/pages/Marketplace/Marketplace.js
+++ build/pages/Marketplace/Marketplace.js
@@ -173,12 +173,17 @@ const Marketplace = ({ allMarketplaceApps, allMarketplaceLabels, allMarketplaceP
                 publisherFiltersForFilterAction.push(publisher.id);
             }
         });
+        const hasFilters = [
+            publisherFiltersForFilterAction,
+            labelFiltersForFilterAction,
+        ].some((f) => f.length);
         getFilteredMarketplaceAppsAction({
             org_id: publisherFiltersForFilterAction,
             label: labelFiltersForFilterAction,
             sort_by: sortModeForFilterAction,
             order: orderModeForFilterAction,
-            page,
+            // if filter changed reset page to 1
+            page: hasFilters ? 1 : page,
             pageSize,
             search: searchTerm.toLowerCase(),
         });
@@ -187,12 +192,22 @@ const Marketplace = ({ allMarketplaceApps, allMarketplaceLabels, allMarketplaceP
         filterAndSortApps({ page, pageSize: APPS_PER_PAGE });
         setFiltersHaveChanged(false);
     }, [filtersHaveChanged, labelFilters, publisherFilters, sortMode]);
+    const [debounceCalled, setDebounceCalled] = useState(false);
     useEffect(() => {
         if (searchTerm.length === 0) {
+            if (debounceCalled) {
+                debounce('MARKETPLACE_FILTER_BY_SEARCH', () => {
+                    // cancel the previous debounce it there was any
+                }, 100);
+                setDebounceCalled(false);
+            }
             filterAndSortApps({ page, pageSize: APPS_PER_PAGE });
         }
         else {
-            debounce('MARKETPLACE_FILTER_BY_SEARCH', () => filterAndSortApps({ page, pageSize: APPS_PER_PAGE }), 1000);
+            debounce('MARKETPLACE_FILTER_BY_SEARCH', 
+            // if search is being used set page to 1
+            () => filterAndSortApps({ page: 1, pageSize: APPS_PER_PAGE }), 1000);
+            setDebounceCalled(true);
         }
     }, [searchTerm]);
     // App's pagination
