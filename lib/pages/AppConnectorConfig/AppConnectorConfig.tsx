1f8fb19f0f8cad788fd6125995dd8289f6586dae
Luis Troni <luistroni@cloudoki.com>
2022-03-11 13:28:22 +0000

    feat: fixes frontend

--- lib/pages/AppConnectorConfig/AppConnectorConfig.tsx
+++ lib/pages/AppConnectorConfig/AppConnectorConfig.tsx
@@ -12,7 +12,7 @@ import {
   FormControlLabel,
   Button,
 } from '@apisuite/fe-base'
-import { getAppConnectorConfigAction } from '../Marketplace/ducks'
+import { getAppConnectorConfigAction, getAppDetailsAction } from '../Marketplace/ducks'
 import useStyles from './styles'
 import appConnectorConfigSelector from './selector'
 import { Alert } from '@material-ui/lab'
@@ -41,7 +41,7 @@ const AppConnectorConfig: React.FC = () => {
   /* Triggers the retrieval and storage (on the app's Store, under 'marketplace > selectedAppDetails')
   of all information we presently have on the currently selected marketplace app. */
   useEffect(() => {
-    if (appID !== '') dispatch(getAppConnectorConfigAction(appID))
+    if (appID !== '') dispatch(getAppDetailsAction(appID))
   }, [appID])
 
   /* The following effect code will check if the currently selected app is one that
@@ -59,96 +59,114 @@ const AppConnectorConfig: React.FC = () => {
     <main>
       <section className={classes.appDetailsContainer}>
         <Grid container spacing={3}>
-          <Grid item xs={8}>
-            <Typography variant="h3">
-              {t('appMarketplace.appConnectorConfig.title', {
-                appName: selectedAppDetails.name,
-              })}
-            </Typography>
-            <Typography variant="body1">
-              {t('appMarketplace.appConnectorConfig.description', {
-                appName: selectedAppDetails.name,
-              })}
-            </Typography>
+          <Grid item xs={12}>
+            <Grid container spacing={3}>
+              <Grid item xs={8}>
+                <Typography variant="h3">
+                  {t('appMarketplace.appConnectorConfig.title', {
+                    appName: selectedAppDetails.name,
+                  })}
+                </Typography>
+                <Typography variant="body1">
+                  {t('appMarketplace.appConnectorConfig.description', {
+                    appName: selectedAppDetails.name,
+                  })}
+                </Typography>
+              </Grid>
+              <Grid item xs={4}>
+                <Alert severity="success">
+                  {t('appMarketplace.appConnectorConfig.alerts.upToDate', {
+                    appName: selectedAppDetails.name,
+                  })}
+                </Alert>
+                <Alert severity="warning">
+                  {t('appMarketplace.appConnectorConfig.alerts.outdated', {
+                    appName: selectedAppDetails.name,
+                  })}
+                </Alert>
+              </Grid>
+            </Grid>
           </Grid>
-          <Grid item xs={4}>
-            <Alert severity="success">
-              {t('appMarketplace.appConnectorConfig.alerts.upToDate', {
-                appName: selectedAppDetails.name,
-              })}
-            </Alert>
-            <Alert severity="success">
-              {t('appMarketplace.appConnectorConfig.alerts.outdated', {
-                appName: selectedAppDetails.name,
-              })}
-            </Alert>
+          <Grid item xs={12}>
+            <Box>
+              <Typography variant="h4">
+                {t('appMarketplace.appConnectorConfig.apiEndpointTitle')}
+              </Typography>
+              <TextField
+                id="api_endpoint"
+                name="api_endpoint"
+                label={t('appMarketplace.appConnectorConfig.apiEndpointLabel')}
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={12}>
+            <Box>
+              <Typography variant="h4">
+                {t('appMarketplace.appConnectorConfig.fieldMatching')}
+              </Typography>
+              <Typography variant="body1">
+                {t(
+                  'appMarketplace.appConnectorConfig.fieldMatchingDescription',
+                  {
+                  appName: selectedAppDetails.name,
+                })}
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={12}>
+            <Grid container spacing={3}>
+              <Grid item xs={5}>
+                <Typography variant="body1">
+                  {t('appMarketplace.appConnectorConfig.appFields', {
+                    appName: selectedAppDetails.name,
+                  })}
+                </Typography>
+              </Grid>
+              <Grid item xs={2}></Grid>
+              <Grid item xs={5}>
+                <Typography variant="body1">
+                  {t('appMarketplace.appConnectorConfig.apiFields')}
+                </Typography>
+              </Grid>
+              <Grid item xs={5}>
+                <TextField
+                  label={t('appMarketplace.appConnectorConfig.appField')}
+                />
+              </Grid>
+              <Grid item xs={2}>
+                <Icon>ArrowRightAlt</Icon>
+              </Grid>
+              <Grid item xs={5}>
+                <TextField
+                  label={t('appMarketplace.appConnectorConfig.apiField')}
+                />
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs={12}>
+            <Box>
+              <Typography variant="h4">
+                {t('appMarketplace.appConnectorConfig.connectionStatus')}
+              </Typography>
+              <Typography variant="body1">
+                {t(
+                  'appMarketplace.appConnectorConfig.connectionStatusDescription'
+                )}
+              </Typography>
+              <FormControlLabel
+                control={<Switch name="checkedA" />}
+                label={t('appMarketplace.appConnectorConfig.connectionOn')}
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={12}>
+            <Box>
+              <Button variant="contained">
+                {t('appMarketplace.appConnectorConfig.back')}
+              </Button>
+            </Box>
           </Grid>
         </Grid>
-        <Box>
-          <Typography variant="h4">
-            {t('appMarketplace.appConnectorConfig.apiEndpointTitle')}
-          </Typography>
-          <TextField
-            id="api_endpoint"
-            name="api_endpoint"
-            label={t('appMarketplace.appConnectorConfig.apiEndpointLabel')}
-          />
-        </Box>
-        <Box>
-          <Typography variant="h4">
-            {t('appMarketplace.appConnectorConfig.fieldMatching')}
-          </Typography>
-          <Typography variant="body1">
-            {t('appMarketplace.appConnectorConfig.fieldMatchingDescription', {
-              appName: selectedAppDetails.name,
-            })}
-          </Typography>
-        </Box>
-        <Grid container spacing={3}>
-          <Grid item xs={5}>
-            <Typography variant="body1">
-              {t('appMarketplace.appConnectorConfig.appFields', {
-                appName: selectedAppDetails.name,
-              })}
-            </Typography>
-          </Grid>
-          <Grid item xs={2}></Grid>
-          <Grid item xs={5}>
-            <Typography variant="body1">
-              {t('appMarketplace.appConnectorConfig.apiFields')}
-            </Typography>
-          </Grid>
-          <Grid item xs={5}>
-            <TextField
-              label={t('appMarketplace.appConnectorConfig.appField')}
-            />
-          </Grid>
-          <Grid item xs={2}>
-            <Icon>ArrowRightAlt</Icon>
-          </Grid>
-          <Grid item xs={5}>
-            <TextField
-              label={t('appMarketplace.appConnectorConfig.apiField')}
-            />
-          </Grid>
-        </Grid>
-        <Box>
-          <Typography variant="h4">
-            {t('appMarketplace.appConnectorConfig.connectionStatus')}
-          </Typography>
-          <Typography variant="body1">
-            {t('appMarketplace.appConnectorConfig.connectionStatusDescription')}
-          </Typography>
-          <FormControlLabel
-            control={<Switch name="checkedA" />}
-            label={t('appMarketplace.appConnectorConfig.connectionOn')}
-          />
-        </Box>
-        <Box>
-          <Button variant="contained">
-            {t('appMarketplace.appConnectorConfig.back')}
-          </Button>
-        </Box>
       </section>
     </main>
   )
